5e6c332bc9f491553b3e949ae4fc6487fd001e3e
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-05-11 16:42:33 +0100

    hard code priority tests

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
-      todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
+      todos: [
+        { name: "Buy shopping", priority: "Low" },
+        { name: "Clean bathroom", priority: "Low" },
+        { name: "Car's MOT", priority: "High" },
+      ],
       newItem: "",
+      picked: "",
     },
     methods: {
       saveNewItem: function () {
         this.todos.push(this.newItem);
         this.newItem = "";
+      },
+      setPriority: function (index) {
+        this.items[index].isPurchased = true;
       },
     },
   });
