a3bbe75cd56209dc4ee8b527833a472d59cc6db3
Martin Graves 422 <33223356+markpackham@users.noreply.github.com>
2020-05-12 16:56:00 +0100

    Initial commit

--- src/app.js
+++ src/app.js
@@ -3,23 +3,9 @@ import Vue from "vue";
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
-    data: {
-      todos: [
-        { name: "Buy shopping", priority: "Low" },
-        { name: "Clean bathroom", priority: "Low" },
-        { name: "Car's MOT", priority: "High" },
-      ],
-      newItem: "",
-      picked: "Low",
-    },
-    methods: {
-      saveNewItem: function () {
-        this.todos.push({
-          name: this.newItem,
-          priority: this.picked,
-        });
-        this.newItem = "";
-      },
-    },
+    data: {},
+    computed: {},
+    mounted() {},
+    methods: {},
   });
 });
